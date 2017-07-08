98bbbabf7cd19d439d47b3486f4dda7d3e76f11e
mskims <mskim@quicket.co.kr>
2017-07-08 19:36:09 +0900

    Show logs

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -24,4 +24,4 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GITHUB_TOKEN}@github.com/mskims/redux-saga-in-korean" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/mskims/redux-saga-in-korean" master:gh-pages
