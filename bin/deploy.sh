797b359efe65e0d6f5f907bba6af162ddd64bc16
mskims <mskim@quicket.co.kr>
2017-07-08 19:18:40 +0900

    Replace build script to npm script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,7 +5,7 @@ set -e # exit with nonzero exit code if anything fails
 rm -rf _book || exit 0;
 
 # run our compile script, discussed above
-gitbook build
+npm run build
 
 # go to the out directory and create a *new* Git repo
 cd _book
